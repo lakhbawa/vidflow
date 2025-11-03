1f5ae7d662cf5e143402c0b7778eaacf5cdfa4c2
Lakh Bawa <lakhveer.bawa@viamo.io>
2025-11-03 15:49:30 -0500

    Feat: file upload working

---
+++ next-frontend/src/app/video/conversion/includes/ConvertVideoForm.tsx
@@ -0,0 +1,70 @@
+"use client";
+import {
+  convertJsonToFormData,
+  handleFormInputChangeHelper,
+} from "@/lib/helpers";
+import axios from "axios";
+import { FormEvent, useState } from "react";
+
+export default function ConvertVideoForm() {
+  console.log("loaded");
+  const [formData, setFormData] = useState({
+    file: null,
+    target_format: "mp3",
+  });
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    handleFormInputChangeHelper(event, setFormData);
+  }
+
+  async function submitForm(event: FormEvent) {
+    event.preventDefault();
+
+    console.log("form submission: ", convertJsonToFormData(formData));
+
+    const res = await axios
+      .post("/api/video/convert", convertJsonToFormData(formData), {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  return (
+    <>
+      <form onSubmit={submitForm} encType="multipart/form-data">
+        <label>
+          <input
+            className="border-gray-200 p-2 border-2 rounded"
+            name="file"
+            type="file"
+            accept=".mp4"
+            placeholder="Select File"
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          <select
+            className="border-gray-200 p-2 border-2 rounded"
+            name="target_format"
+            onChange={handleInputChange}
+          >
+            <option value="mp3">Mp3</option>
+          </select>
+        </label>
+
+        <input
+          className="border-gray-200 p-2 border-2 rounded bg-gray-500 text-white"
+          type="submit"
+          value="Submit"
+          onClick={submitForm}
+        ></input>
+      </form>
+    </>
+  );
+}
---
+++ next-frontend/src/app/video/conversion/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import ConvertVideoForm from "./includes/ConvertVideoForm"
+
+
+export default function Conversion() {
+
+    return (
+        <>
+        Video Conversion
+        <ConvertVideoForm></ConvertVideoForm>
+        </>
+    )
+}
